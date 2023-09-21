467ab58377cfe6292903794a06efe451f230a5b4
Anuj_Maurya <am398@snu.edu.in>
2023-09-21 21:25:43 +0530

    Create Community

---
+++ src/components/Navbar/Directory/Directory.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineLogin } from "react-icons/md";
+import { GrAdd } from "react-icons/gr";
+import { TiHome } from "react-icons/ti";
+import Communities from "./Communities";
+
+type DirectoryProps = {};
+
+const Directory: React.FC<DirectoryProps> = () => {
+  return (
+    <Menu>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius="4px"
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+        mr={2}
+        ml={2}
+      >
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          width={{ base: "auto", lg: "200px" }}
+        >
+          <Flex alignItems="center">
+            <>
+              <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+              <Box
+                display={{ base: "none", lg: "flex" }}
+                flexDirection="column"
+                fontSize="10pt"
+              >
+                <Text fontWeight={600}>Home</Text>
+              </Box>
+            </>
+          </Flex>
+          <ChevronDownIcon color="gray.500" />
+        </Flex>
+      </MenuButton>
+      <MenuList width="100%">
+        <Communities/>
+        {/* <> */}
+          {/* <Flex direction="column" mt={2} mb={4}> */}
+          {/* <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
+            MY COMMUNITIES
+          </Text>
+          <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
+            <Flex alignItems="center">
+              <Icon fontSize={20} mr={2} as={GrAdd} />
+              Create Community
+            </Flex>
+          </MenuItem> */}
+          {/* </Flex> */}
+          {/* <Text pl={3} fontSize="7pt" fontWeight={500} color="gray.500">
+            FEEDS
+          </Text>
+          <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
+            <Flex alignItems="center">
+              <Icon fontSize={20} mr={2} as={GrAdd} />
+              Create Community
+            </Flex>
+          </MenuItem> */}
+        {/* </> */}
+      </MenuList>
+    </Menu>
+  );
+};
+export default Directory;
---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,107 @@
+import { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import type { GetServerSidePropsContext, NextPage } from "next";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState } from "recoil";
+import safeJsonStringify from "safe-json-stringify";
+import { Community, communityState } from "../../../atoms/communitiesAtom";
+// import About from "../../../components/Community/About";
+// import CommunityNotFound from "../../../components/Community/CommunityNotFound";
+// import CreatePostLink from "../../../components/Community/CreatePostLink";
+// import Header from "../../../components/Community/Header";
+// import PageContentLayout from "../../../components/Layout/PageContent";
+// import Posts from "../../../components/Post/Posts";
+import { auth, firestore } from "../../../firebase/clientApp";
+import CommunityNotFound from "../../../components/Community/NotFound";
+
+interface CommunityPageProps {
+  communityData: Community;
+}
+
+const CommunityPage: NextPage<CommunityPageProps> = ({communityData}) => {
+  console.log(communityData);
+  // const [user, loadingUser] = useAuthState(auth);
+
+  // const [communityStateValue, setCommunityStateValue] =
+  //   useRecoilState(communityState);
+
+  // useEffect(() => {
+  //   // First time the user has navigated to this community page during session - add to cache
+  //   const firstSessionVisit =
+  //     !communityStateValue.visitedCommunities[communityData.id!];
+
+  //   if (firstSessionVisit) {
+  //     setCommunityStateValue((prev) => ({
+  //       ...prev,
+  //       visitedCommunities: {
+  //         ...prev.visitedCommunities,
+  //         [communityData.id!]: communityData,
+  //       },
+  //     }));
+  //   }
+  // }, [communityData]);
+
+  // useEffect(() => {
+  //   setCommunityStateValue((prev) => ({
+  //     ...prev,
+  //     currentCommunity: communityData,
+  //   }));
+  // }, [communityData, setCommunityStateValue]);
+
+  // Community was not found in the database
+  if (!communityData) {
+    return <CommunityNotFound />;
+  }
+
+  return (
+    <div>
+        Welcome to {communityData.id}
+    </div>
+    // <>
+    //   <Header communityData={communityData} />
+    //   <PageContentLayout>
+    //     {/* Left Content */}
+    //     <>
+    //       <CreatePostLink />
+    //       <Posts
+    //         communityData={communityData}
+    //         userId={user?.uid}
+    //         loadingUser={loadingUser}
+    //       />
+    //     </>
+    //     {/* Right Content */}
+    //     <>
+    //       <About communityData={communityData} />
+    //     </>
+    //   </PageContentLayout>
+    // </>
+  );
+};
+
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() }) // needed for dates
+            )
+          : "",
+      },
+    };
+  } catch (error) {
+    console.log("getServerSideProps error - [community]", error);
+    return { props: {} };
+  }
+}
+
+export default CommunityPage;
